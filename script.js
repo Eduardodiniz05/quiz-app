69c70f6ce9c373a4ca8d4b30c568daa981034a01
Eduardodiniz05 <eduardo.diniz005@gmail.com>
2021-10-08 16:05:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+const quizData = [
+    {
+        Questao: 'De quem é a famosa frase “Penso, logo existo”?',
+        a: "Platão",
+        b: "Galileu Galilei",
+        c: "Descartes",
+        d: "Sócrates",
+        e: "Francis Bacon",
+        Correta: 'René Descartes'
+    },
+    {
+        Questao: "De onde é a invenção do chuveiro elétrico?",
+       a: 'França',
+       b: 'Inglaterra',
+       c: 'Brasil',
+       d: 'Austrália',
+       e: 'Itália',
+        Correta: 'Brasil'
+    },
+    {
+        Questao: 'Quais o menor e o maior país do mundo?',
+       a: "Vaticano e Rússia",
+       b: "Nauru e China",
+       c: "Mônaco e Canadá",
+       d: "Malta e Estados Unidos",
+       e: "São Marino e Índia",
+        Correta: "Vaticano e Rússia"
+    },
+    {
+       Questao: "Qual o nome do presidente do Brasil que ficou conhecido como Jango?",
+       a: "Jânio Quadros",
+       b: "Jacinto Anjos",
+       c: "Getúlio Vargas",
+       d: "João Figueiredo",
+       e: "João Goulart",
+       Correta: "João Goulart"
+    },
+    {
+        Questao: "Qual o livro mais vendido no mundo a seguir à Bíblia?",
+       a: "O Senhor dos Anéis",
+       b: "Dom Quixote",
+       c: "O Pequeno Príncipe",
+       d: "Ela, a Feiticeira",
+       e: "Um Conto de Duas Cidades",
+       Correta: "Dom Quixote"
+    },
+    {
+        Questao: 'Quantas casas decimais tem o número pi?',
+       a: "Duas",
+       b: "Centenas",
+       c: "Infinitas",
+       d: "Vinte",
+       e: "Milhares",
+       Correto: 'Infinitas'
+    },
+    {
+        Questao: 'Atualmente, quantos elementos químicos a tabela periódica possui?',
+        a: '113',
+        b: '109',
+        c: '108',
+        d: '118',
+        e: '92',
+        Correto: '118'
+    }
+]
